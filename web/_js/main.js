9ff8d6203bf1bc7297fbb82386a799eae5b0dc8a
Wolfgang <wgehner@gmail.com>
2017-08-20 13:08:30 -0700

    Added Webcomps

---
+++ web/_js/main.js
@@ -0,0 +1,33 @@
+'use strict'
+loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
+	success: function(){
+	loadjs([
+		//'/_js/vendor/jquery.jsForm.min.js'
+        '//cdn.jsdelivr.net/gsap/1.19.0/TweenMax.min.js'
+        ,'//cdn.jsdelivr.net/gsap/1.19.0/jquery.gsap.min.js'
+        ,'//cdn.jsdelivr.net/jquery.fullpage/2.8.9/jquery.fullpage.min.js'
+
+		], { success: function(){
+			libsLoaded()
+		}
+	})//loadjs
+	}//suc
+})
+
+//========================================================
+function libsLoaded(){
+	
+	TS.signalAppReady()
+
+	TT.ScontentID = "#fullpage"
+	//TT.ScontentID = '#content-wrapper'
+	TT.handle(function(evt) {
+		if (TT.PRE == evt.typ)  {//start
+			$('#fullpage').fadeTo(100,.2)
+		}
+		if (TT.PAGE == evt.typ)  {//new pg loaded
+			//$(TT.ScontentID).html(evt.$new)
+			$('#fullpage').fadeTo(100,1)
+		}
+	})
+}
